abfa185f812a969f4bdda92c7ef055503f100d11
Jamuel <jammmg26@gmail.com>
2020-12-27 23:48:15 +0800

    long lived access tokens

--- client/src/actions/facebook.js
+++ client/src/actions/facebook.js
@@ -4,5 +4,6 @@ import { message } from "antd";
 export const getFbTokens = (fbCreds) => async (dispatch) => {
   const data = await axios.post("/api/facebook/pageTokens", fbCreds);
   dispatch({ type: "FB_PAGE_TOKENS", payload: data.data });
+
   message.info("Authorized");
 };
--- controllers/facebook.js
+++ controllers/facebook.js
@@ -3,9 +3,16 @@ const subscribe = require("../facebook/functions/subscribe_to_apps");
 
 exports.getFbTokens = async (req, res, next) => {
   const { accessToken, userID } = req.body.authResponse;
+
+  const long_token = await axios.get(
+    `https://graph.facebook.com/v9.0/oauth/access_token?grant_type=fb_exchange_token&client_id=${process.env.FB_APP_ID}&client_secret=${process.env.FB_APP_SECRET}&fb_exchange_token=${accessToken}`
+  );
+
+  const { access_token } = long_token.data;
+
   try {
     const data = await axios.get(
-      `https://graph.facebook.com/${userID}/accounts?access_token=${accessToken}`
+      `https://graph.facebook.com/v9.0/${userID}/accounts?access_token=${access_token}`
     );
 
     res.send(data.data.data);
