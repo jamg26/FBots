3654d2a28b19ec565cc5888a667da44112ecff26
Jamuel <jammmg26@gmail.com>
2020-12-29 20:09:50 +0800

    re

--- facebook/functions/get_info.js
+++ facebook/functions/get_info.js
@@ -1,18 +1,19 @@
 const axios = require("axios");
 const getToken = require("./page_token");
 module.exports = async (psid) => {
+  const token = await getToken();
   try {
-    const token = await getToken();
     const user = await axios.get(
       `https://graph.facebook.com/${psid}?fields=first_name,last_name,profile_pic&access_token=${token}`
     );
-    const { first_name, last_name, profile_pic } = user.data;
-    return {
-      first_name: first_name ? first_name : "-",
-      last_name: last_name ? last_name : "-",
-      profile_pic: profile_pic ? profile_pic : "-",
-    };
+    return user.data;
   } catch (error) {
+    console.log("CANT GET INFO:", psid, token);
     console.log("ERROR GET_INFO: ", error.message);
+    return {
+      first_name: "-",
+      last_name: "-",
+      profile_pic: "-",
+    };
   }
 };
