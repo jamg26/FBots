21c184be9f7e93b906437d4d7328f4c189ba55b4
Jamuel <jammmg26@gmail.com>
2021-01-26 22:39:35 +0800

    re

--- facebook/listeners/text.js
+++ facebook/listeners/text.js
@@ -52,9 +52,9 @@ module.exports = async (senderID, messageText, pageID) => {
     const page = await Pages.findOne({ pageid: temp_db.page_id });
     const settings = await Settings.findOne({ author: page.author });
 
-    console.log(customer);
-    if (Object.keys(customer).length === 0 && customer.constructor === Object)
-      return requestName();
+    console.log("==>", customer);
+    console.log("==>", typeof customer);
+    if (customer == null) return requestName();
 
     db.orders.forEach(async (s) => {
       if (s.sender === senderID) {
