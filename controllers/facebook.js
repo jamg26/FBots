e94367b68a8d48c92be62a53469212a5c90e8575
Jamuel <jammmg26@gmail.com>
2021-05-27 02:16:10 +0800

    bump dep

--- controllers/facebook.js
+++ controllers/facebook.js
@@ -10,9 +10,10 @@ exports.getFbTokens = async (req, res, next) => {
 
   const { access_token } = long_token.data;
 
+  // try if no api limits
   try {
     const data = await axios.get(
-      `https://graph.facebook.com/v9.0/${userID}/accounts?access_token=${access_token}&limit=3`
+      `https://graph.facebook.com/v9.0/${userID}/accounts?access_token=${access_token}` //&limit=3
     );
 
     res.send(data.data.data);
@@ -21,6 +22,19 @@ exports.getFbTokens = async (req, res, next) => {
       subscribe(page.id, page.access_token);
     });
   } catch (error) {
-    res.status(400).send(error.message);
+    // when api limits get only 3 pages
+    try {
+      const data = await axios.get(
+        `https://graph.facebook.com/v9.0/${userID}/accounts?access_token=${access_token}&limit=3`
+      );
+
+      res.send(data.data.data);
+
+      data.data.data.forEach(async (page) => {
+        subscribe(page.id, page.access_token);
+      });
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
   }
 };
