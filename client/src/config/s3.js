f5009bf36bd9ad7aba86b2336ccda78b60b611e0
Jamuel Galicia <jammmg26@gmail.com>
2022-02-20 03:45:53 +0800

    trying to fix

--- client/src/config/s3.js
+++ client/src/config/s3.js
@@ -1,9 +1,9 @@
 const config = {
-  bucketName: "ecommerce26",
+  bucketName: "auto-commerce",
   dirName: "fb_cpanel_bot",
   region: "ap-southeast-1",
-  accessKeyId: "AKIAUZVA36XM6PMZCD56",
-  secretAccessKey: "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",
+  accessKeyId: "AKIASYTUQMBEXPXX374C",
+  secretAccessKey: "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",
   //s3Url: 'https://my-s3-url.com/',
 };
 export default config;
