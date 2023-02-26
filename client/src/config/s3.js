2fc6565151c72099737aabf54ea240ffdea56811
Jamuel Galicia <44177731+jamg26@users.noreply.github.com>
2023-02-26 12:12:14 +0800

    Update s3.js

--- client/src/config/s3.js
+++ client/src/config/s3.js
@@ -2,8 +2,8 @@ const config = {
   bucketName: "auto-commerce",
   dirName: "fb_cpanel_bot",
   region: "ap-southeast-1",
-  accessKeyId: "AKIASYTUQMBEXPXX374C",
-  secretAccessKey: "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",
+  accessKeyId: "",
+  secretAccessKey: "",
   //s3Url: 'https://my-s3-url.com/',
 };
 export default config;
